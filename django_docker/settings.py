c79c6edff168f04cd4912bcc65c78f5e80c369f5
hwang-in-kyu <ic663688@gmail.com>
2026-04-28 09:51:46 +0900

    settings변경

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '43.202.194.241'
+]
 
 
 # Application definition
